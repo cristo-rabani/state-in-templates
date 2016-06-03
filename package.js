5d3c494e21bdd21c68a4f428f1bce6b5ccc25fe2
cristo-rabani <cristo.rabani@gmail.com>
2016-06-03 17:05:20 +0200

    up

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cristo:state-in-templates',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'this package brings state functionality to the blaze templates',
   // URL to the Git repository containing the source code for this package.
@@ -13,5 +13,5 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.3.2.4');
   api.use(['ecmascript', 'templating', 'reactive-var', 'reactive-dict'], 'client');
-  api.mainModule('state-in-templates.js');
+  api.mainModule('state-in-templates.js', 'client');
 });
